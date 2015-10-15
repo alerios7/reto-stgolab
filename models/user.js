5927d6a30f6a63d6f80e21b7e03c77068523ea25
Alejandro Ríos <alerios7@gmail.com>
2015-10-15 18:33:06 -0300

    creacion de esquema y conexion con base de datos

---
+++ models/user.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var User = new Schema ({
+    email: {
+        type: String,
+        required: "Debes ingresar un email!",
+        validate: {
+            validator: function (v) {
+                return /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i.test(v);
+            },
+            message: "Debes ingresar un email válido"
+        }
+    },
+    color: {
+        type: String,
+        required: "Debes seleccionar un color",
+        validate: {
+            validator: function (v) {
+                return /\b(verde|azul)$/i.test(v);
+            },
+            message: "Debes seleccionar verde o azul"
+        }
+    },
+    number: {
+        type: Number,
+        required: "Debes seleccionar un número (1, 2 o 3)",
+        min: 1,
+        max: 3
+    }
+});
+
+module.exports = mongoose.model('User', User);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
